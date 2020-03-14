24a9a412517c03c5b9f575cb5e1c381bfd81f203
hdq <haqsir@163.com>
2020-03-14 23:00:07 +0800

    readme

--- src/index.js
+++ src/index.js
@@ -23,7 +23,3 @@ const theme = localStorage.getItem('theme')
 theme && changeTheme(theme)
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
