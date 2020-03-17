33594fab830da8bf0fb544dcda87a7436e70b479
hdq <haqsir@163.com>
2020-03-17 22:18:21 +0800

    storage

--- color.js
+++ color.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const { generateTheme, getLessVars } = require('antd-theme-generator');
+const { generateTheme } = require('antd-theme-generator');
 
 const options = {
     stylesDir: path.join(__dirname, './src/style'),
     antDir: path.join(__dirname, './node_modules/antd'),
     varFile: path.join(__dirname, './src/styles/vars.less'),
-    // mainLessFile: path.join(__dirname, './src/styles/main.less'),
     themeVariables: [
         '@primary-color',
         '@secondary-color',
